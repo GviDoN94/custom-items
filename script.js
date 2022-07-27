95dd48f79be4b6e9db31335c5e4a8706ec091b1a
Vladislav Surin <vlad_surin@mail.ru>
2022-07-27 18:39:04 +0300

    added yandex map

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// const selectEl = document.querySelector('#select');
+// const choices = new Choices(selectEl, {
+//   removeItems: false,
+//   searchEnabled: false,
+//   position: 'bottom',
+//   shouldSort: false,
+//   itemSelectText: '',
+// });
+
+ymaps.ready(init);
+function init() {
+  const yandexMap = new ymaps.Map("yandexMap", {
+    center: [48.872185073737896, 2.354223999999991],
+    zoom: 11,
+  });
+
+  // const mapPoint = new ymaps.GeoObject({
+  //   geometry: {
+  //     type: "Point", // тип геометрии - точка
+  //     coordinates: [48.872185073737896, 2.354223999999991],
+  //   },
+  // });
+  const mapPoint = new ymaps.Placemark(
+    [48.872185073737896, 2.354223999999991],
+    {},
+    {
+      iconLayout: "default#image",
+      iconImageHref: "img/mapPoint.svg",
+      iconImageSize: [28, 40],
+    }
+  );
+
+  yandexMap.geoObjects.add(mapPoint);
+}
